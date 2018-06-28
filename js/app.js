0060bfcda42f94aba1b6a8911b6d3bcb499b17eb
marek sadecky <sadecky.marek@gmail.com>
2018-06-28 14:26:40 +0200

    initial commit with workflow for project

--- js/app.js
+++ js/app.js
@@ -1,18 +1,11 @@
-/*
- * Create a list that holds all of your cards
- */
+//selectors and array of cards
 
-
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+const cardsArray= ['ambulance','camera-retro','bug','child','hourglass-start', 'android','fighter-jet', 'gift','ambulance','camera-retro','bug','child','hourglass-start', 'android','fighter-jet', 'gift'];
+$deck = $('.deck');
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    let currentIndex = array.length, temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -25,14 +18,18 @@ function shuffle(array) {
     return array;
 }
 
+//Generating cards for Index HTML with class attributes and font awesome icons
+function generateGame() {
+    let startingCards = shuffle(cardsArray);
+    for (let i = 0; i < startingCards.length; i++) {
+        $deck.append($('<li class="card"><i class="fa fa-' + startingCards[i] + '"></i></li>'))
+    }
+}
+//function startOfGame() {
+//    let cards = shuffle(icons);
+//   for (let x = 0; x < cards.length; x++) {
+//        $deck.append($('<li class="card"><x class="fa fa-' + cards[x] + '"></x></li>'))
+//    }
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+generateGame();
+
